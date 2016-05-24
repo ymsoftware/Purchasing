753f83761989d72a9acd3abc3b81473457fe1b43
ymetelkin <ymetelkin@ap.org>
2016-05-24 16:14:52 -0400

    Work in progress

---
+++ YM.Purchasing.Tests/GroupCollectionTests.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace YM.Purchasing.Tests
+{
+    [TestClass]
+    public class GroupCollectionTests
+    {
+        [TestMethod]
+        public void Add_items_must_simply_increment()
+        {
+            var col = GetCollection();
+            object result = col.Invoke("AddItem", "1");
+
+            string[] items = (string[])col.GetProperty("Items");
+            Assert.IsTrue(items.Length == 1);
+            Assert.IsTrue(items[0] == "1");
+
+            col.Invoke("AddItem", "2");
+            items = (string[])col.GetProperty("Items");
+            Assert.IsTrue(items.Length == 2);
+            Assert.IsTrue(items[1] == "2");
+
+            col.Invoke("AddItem", "3");
+            items = (string[])col.GetProperty("Items");
+            Assert.IsTrue(items.Length == 3);
+            Assert.IsTrue(items[2] == "3");
+
+            string[] groups = (string[])col.GetProperty("Groups");
+            Assert.IsTrue(groups.Length == 0);
+        }
+
+        [TestMethod]
+        public void Remove_items_must_simply_decrement()
+        {
+            var col = GetCollection();
+            object result = col.Invoke("AddItem", "1");
+
+            string[] items = (string[])col.GetProperty("Items");
+            Assert.IsTrue(items.Length == 1);
+            Assert.IsTrue(items[0] == "1");
+
+            col.Invoke("AddItem", "2");
+            items = (string[])col.GetProperty("Items");
+            Assert.IsTrue(items.Length == 2);
+            Assert.IsTrue(items[1] == "2");
+
+            col.Invoke("AddItem", "3");
+            items = (string[])col.GetProperty("Items");
+            Assert.IsTrue(items.Length == 3);
+            Assert.IsTrue(items[2] == "3");
+
+            col.Invoke("RemoveItem", 1);
+            items = (string[])col.GetProperty("Items");
+            Assert.IsTrue(items.Length == 2);
+            Assert.IsTrue(items[1] == "3");
+
+            string[] groups = (string[])col.GetProperty("Groups");
+            Assert.IsTrue(groups.Length == 0);
+        }
+
+        [TestMethod]
+        public void Add_groups_must_simply_increment()
+        {
+            var col = GetCollection();
+            object result = col.Invoke("AddGroup", "1");
+
+            string[] groups = (string[])col.GetProperty("Groups");
+            Assert.IsTrue(groups.Length == 1);
+            Assert.IsTrue(groups[0] == "1");
+
+            col.Invoke("AddGroup", "2");
+            groups = (string[])col.GetProperty("Groups");
+            Assert.IsTrue(groups.Length == 2);
+            Assert.IsTrue(groups[1] == "2");
+
+            string[] items = (string[])col.GetProperty("Items");
+            Assert.IsTrue(items.Length == 0);
+        }
+
+        [TestMethod]
+        public void Add_items_to_groups_must_rearrange()
+        {
+            var col = GetCollection();
+
+            col.Invoke("AddGroup", "1");
+            col.Invoke("AddGroup", "2");
+            string[] groups = (string[])col.GetProperty("Groups");
+            Assert.IsTrue(groups.Length == 2);
+            Assert.IsTrue(groups[0] == "1");
+            Assert.IsTrue(groups[1] == "2");
+
+            col.Invoke("AddItem", "1", 0);
+            col.Invoke("AddItem", "2", 1);
+            col.Invoke("AddItem", "3", 0);
+
+            string[] items = (string[])col.GetProperty("Items");
+            Assert.IsTrue(items.Length == 3);
+            Assert.IsTrue(items[0] == "1");
+            Assert.IsTrue(items[1] == "3");
+            Assert.IsTrue(items[2] == "2");
+
+            col = GetCollection();
+
+            col.Invoke("AddItem", "1");
+
+            col.Invoke("AddGroup", "1");
+            col.Invoke("AddGroup", "2");
+            groups = (string[])col.GetProperty("Groups");
+            Assert.IsTrue(groups.Length == 2);
+            Assert.IsTrue(groups[0] == "1");
+            Assert.IsTrue(groups[1] == "2");
+            
+            col.Invoke("AddItem", "2", 1);
+            col.Invoke("AddItem", "3", 0);
+
+            items = (string[])col.GetProperty("Items");
+            Assert.IsTrue(items.Length == 3);
+            Assert.IsTrue(items[0] == "3");
+            Assert.IsTrue(items[1] == "2");
+            Assert.IsTrue(items[2] == "1");
+        }
+
+        [TestMethod]
+        public void Moving_items_must_rearrange()
+        {
+            var col = GetCollection();
+
+            col.Invoke("AddItem", "1"); //no group
+            col.Invoke("AddItem", "2"); //no group
+
+            col.Invoke("AddGroup", "1");
+            col.Invoke("AddGroup", "2");
+            var groups = (string[])col.GetProperty("Groups");
+            Assert.IsTrue(groups.Length == 2);
+            Assert.IsTrue(groups[0] == "1");
+            Assert.IsTrue(groups[1] == "2");
+
+            col.Invoke("AddItem", "3", 0); //group 1
+            col.Invoke("AddItem", "4", 0); //group 1
+            col.Invoke("AddItem", "5", 1); //group 2
+            col.Invoke("AddItem", "6", 1); //group 2
+
+            var items = (string[])col.GetProperty("Items");
+            Assert.IsTrue(items.Length == 6);
+            Assert.IsTrue(items[0] == "3");
+            Assert.IsTrue(items[1] == "4");
+            Assert.IsTrue(items[2] == "5");
+            Assert.IsTrue(items[3] == "6");
+            Assert.IsTrue(items[4] == "1");
+            Assert.IsTrue(items[5] == "2");
+
+            col.Invoke("MoveItem", 4, 0); //item 1 to group 1
+            col.Invoke("MoveItem", 5, 1); //item 2 to group 2
+            items = (string[])col.GetProperty("Items");
+            Assert.IsTrue(items[2] == "1");
+            Assert.IsTrue(items[5] == "2");
+
+            col.Invoke("MoveItem", 0, 0, 1); //item 3 to group 2
+            col.Invoke("MoveItem", 0, 0, 1); //item 4 to group 2
+            items = (string[])col.GetProperty("Items");
+            Assert.IsTrue(items[0] == "1");
+            Assert.IsTrue(items[3] == "2");
+            Assert.IsTrue(items[4] == "3");
+            Assert.IsTrue(items[5] == "4");
+
+            col.Invoke("RemoveItem", 0, 0); //remove item 1
+            items = (string[])col.GetProperty("Items");
+            Assert.IsTrue(items.Length == 5);
+            Assert.IsTrue(items[0] == "5");
+            Assert.IsTrue(items[2] == "2");
+            Assert.IsTrue(items[3] == "3");
+            Assert.IsTrue(items[4] == "4");
+
+            col.Invoke("RemoveGroup", 0, 0); //remove group 1; item 3 will move to "no group"
+            items = (string[])col.GetProperty("Items");
+            Assert.IsTrue(items.Length == 5);
+            Assert.IsTrue(items[0] == "5");
+            Assert.IsTrue(items[1] == "6");
+            Assert.IsTrue(items[2] == "2");
+            Assert.IsTrue(items[3] == "3");
+            Assert.IsTrue(items[4] == "4");
+        }
+        
+        private PrivateObject GetCollection()
+        {
+            var asm = typeof(IRequisition).Assembly;
+            var type = asm.GetType("YM.Purchasing.Common.GroupCollection`2");
+            var genType = type.MakeGenericType(new Type[2] { typeof(string), typeof(string) });
+            var obj = Activator.CreateInstance(genType, true);
+            return new PrivateObject(obj);
+        }
+    }
+}
---
+++ YM.Purchasing/Common/GroupCollection.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YM.Purchasing.Common
+{
+    class GroupCollection<TItem,TGroup>
+    {
+        private List<GroupCollectionGroup<TGroup>> _groups;
+        private List<GroupCollectionItem<TItem>> _items;
+
+        public GroupCollection<TItem, TGroup> AddItem(TItem item)
+        {
+            EnsureItems();
+
+            var add = new GroupCollectionItem<TItem>()
+            {
+                Item = item,
+                Index = _items.Count,
+                GroupIndex = -1
+            };
+
+            _items.Add(add);
+
+            return this;
+        }
+
+        public GroupCollection<TItem, TGroup> AddItem(TItem item, int groupIndex)
+        {
+            EnsureItems();
+            EnsureGroups();
+
+            if (groupIndex < 0 || groupIndex >= _groups.Count)
+            {
+                throw new ArgumentException(string.Format("invalid index {0}", groupIndex));
+            }
+
+            var add = new GroupCollectionItem<TItem>()
+            {
+                Item = item,
+                GroupIndex = groupIndex
+            };
+
+            var first = _items.FirstOrDefault(e => e.GroupIndex > groupIndex || e.GroupIndex == -1);
+            if (first == null)
+            {
+                add.Index = _items.Count;
+                _items.Add(add);
+            }
+            else
+            {
+                add.Index = first.Index;
+
+                foreach (var i in _items.Where(e => e.Index >= add.Index))
+                {
+                    i.Index++;
+                }
+
+                _items.Insert(add.Index, add);
+            }                       
+
+            var group = _groups[groupIndex];
+            group.ItemCount++; 
+
+            return this;
+        }
+
+        public GroupCollection<TItem, TGroup> MoveItem(int itemIndex, int groupIndex)
+        {
+            EnsureItems();
+            EnsureGroups();
+
+            if (itemIndex < 0 || itemIndex >= _items.Count)
+            {
+                throw new ArgumentException(string.Format("invalid index {0}", itemIndex));
+            }
+
+            if (groupIndex < 0 || groupIndex > _groups.Count)
+            {
+                throw new ArgumentException(string.Format("invalid index {0}", groupIndex));
+            }
+
+            var item = _items[itemIndex];
+            if (item.GroupIndex != groupIndex)
+            {
+                RemoveItem(itemIndex);
+                return AddItem(item.Item, groupIndex);
+            }
+
+            return this;
+        }
+
+        public GroupCollection<TItem, TGroup> RemoveItem(int itemIndex)
+        {
+            EnsureItems();
+
+            if (itemIndex < 0 || itemIndex >= _items.Count)
+            {
+                throw new ArgumentException(string.Format("invalid index {0}", itemIndex));
+            }
+
+            bool reorder = itemIndex < _items.Count - 1;
+            int groupIndex = _items[itemIndex].GroupIndex;
+
+            _items.RemoveAt(itemIndex);
+
+            if (reorder)
+            {
+                foreach (var i in _items.Where(e => e.Index >= itemIndex))
+                {
+                    i.Index--;
+                }
+            }
+
+            if (groupIndex >= 0)
+            {
+                EnsureGroups();
+
+                _groups.First(e => e.Index == itemIndex).ItemCount--;
+            }
+
+            return this;
+        }
+
+        public GroupCollection<TItem, TGroup> AddGroup(TGroup group)
+        {
+            EnsureGroups();
+            EnsureItems();
+
+            var add = new GroupCollectionGroup<TGroup>()
+            {
+                Group = group,
+                Index = _groups.Count
+            };
+
+            _groups.Add(add);
+
+            return this;
+        }
+
+        public GroupCollection<TItem, TGroup> RemoveGroup(int groupIndex)
+        {
+            EnsureGroups();
+
+            if (groupIndex < 0 || groupIndex > _groups.Count)
+            {
+                throw new ArgumentException(string.Format("invalid index {0}", groupIndex));
+            }
+
+            bool reorder = groupIndex < _groups.Count - 1;
+
+            _groups.RemoveAt(groupIndex);
+
+            EnsureItems();
+
+            var move = _items.Where(e => e.GroupIndex == groupIndex).ToArray();
+            foreach (var item in move)
+            {
+                _items.RemoveAt(item.Index);
+                item.GroupIndex = -1;
+                _items.Add(item);
+            }
+
+            return this;
+        }
+
+        public TItem[] Items
+        {
+            get
+            {
+                EnsureItems();
+                return _items.Select(e => e.Item).ToArray();
+            }
+        }
+
+        public TGroup[] Groups
+        {
+            get
+            {
+                EnsureGroups();
+                return _groups.Select(e => e.Group).ToArray();
+            }
+        }
+
+        private void EnsureItems()
+        {
+            if (_items == null)
+            {
+                _items = new List<GroupCollectionItem<TItem>>();
+            }
+        }
+
+        private void EnsureGroups()
+        {
+            if (_groups == null)
+            {
+                _groups = new List<GroupCollectionGroup<TGroup>>();
+            }
+        }
+    }
+
+    class GroupCollectionItem<T>
+    {
+        public T Item { get; set; }
+        public int Index { get; set; }
+        public int GroupIndex { get; set; }
+    }
+
+    class GroupCollectionGroup<T>
+    {
+        public T Group { get; set; }
+        public int Index { get; set; }
+        public int ItemCount { get; set; }
+    }
+}
